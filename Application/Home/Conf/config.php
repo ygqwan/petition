0f6cb9b30ca4a1dbd23da33b5252cd4cc1e795fe
luotonglong <luotonglong@domob.cn>
2016-12-06 13:15:34 +0800

    'db'

--- Application/Home/Conf/config.php
+++ Application/Home/Conf/config.php
@@ -1,11 +1,11 @@
 <?php
 return array(
 	'DB_TYPE'   => 'mysql', // 数据库类型
-	'DB_HOST'   => '192.168.200.151', // 服务器地址
+	'DB_HOST'   => 'domob-206.domob-inc.cn', // 服务器地址
 	'DB_NAME'   => 'firstblood_petition', // 数据库名
-	'DB_USER'   => 'root', // 用户名
-	'DB_PWD'    => '', // 密码
-	'DB_PORT'   => 3306, // 端口
+	'DB_USER'   => 'oyl', // 用户名
+	'DB_PWD'    => '123456', // 密码
+	'DB_PORT'   => 9987, // 端口
 	'DB_PREFIX' => '', // 数据库表前缀 
 	'DB_CHARSET'=> 'utf8', // 字符集
 	'DB_DEBUG'  =>  TRUE, // 数据库调试模式 开启后可以记录SQL日志 3.2.3新增
