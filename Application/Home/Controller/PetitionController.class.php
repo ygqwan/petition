8382b1b1bff796047fd255ef7f27a2d5f819852a
luotonglong <luotonglong@domob.cn>
2016-12-07 10:33:31 +0800

    crlf -> lf

--- Application/Home/Controller/PetitionController.class.php
+++ Application/Home/Controller/PetitionController.class.php
@@ -34,92 +34,15 @@ class PetitionController extends BaseController {
     public function detail() {
         $res = D("Petition")->petition(I("get.id"));
         echo $this->jsonFromModel($res);
-        die;
-        $petitionInfo = array(
-            'petition' => array(
-                'id' => 1,
-                'title' => '请改善网络情况',
-                'desc' => '希望改善网络情况',
-                'owner' => 'luotonglong@domob.cn',
-                'create_time' => '2016-12-06',
-                'follower' => array('xuqian@domob.cn','lufugang@domob.cn'),
-                'vote_num' => 100,
-            ),
-            'is_mine' => false,
-            'is_follower' => true,
-            'expire_time_left' => 1,
-        );
-        echo $this->defaultSuccessJson($petitionInfo);
     }
 
     public function running() {
-        $petitionInfoList = array(
-            array(
-                'petition' => array(
-                    'id' => 1,
-                    'title' => '请改善网络情况',
-                    'desc' => '希望改善网络情况',
-                    'owner' => 'luotonglong@domob.cn',
-                    'create_time' => '2016-12-06',
-                    'follower' => array('xuqian@domob.cn','lufugang@domob.cn'),
-                    'vote_num' => 100,
-                ),
-                'is_mine' => false,
-                'is_follower' => true,
-                'expire_time_left' => 1,
-            ),
-            array(
-                'petition' => array(
-                    'id' => 2,
-                    'title' => '请改善网络情况2',
-                    'desc' => '希望改善网络情况2',
-                    'owner' => 'lufugang@domob.cn',
-                    'create_time' => '2016-12-06',
-                    'follower' => array('xuqian@domob.cn','luotonglong@domob.cn'),
-                    'vote_num' => 100,
-                ),
-                'is_mine' => true,
-                'is_follower' => false,
-                'expire_time_left' => 1,
-            ),
-        );
-        echo $this->defaultSuccessJson($petitionInfoList);
+        $res = D("Petition")->running();
+        echo $this->jsonFromModel($res);
     }
 
     public function history() {
         $res = D("Petition")->history();
         echo $this->jsonFromModel($res);
-        die;
-        $petitionInfoList = array(
-            array(
-                'petition' => array(
-                    'id' => 1,
-                    'title' => '请改善网络情况',
-                    'desc' => '希望改善网络情况',
-                    'owner' => 'luotonglong@domob.cn',
-                    'create_time' => '2016-12-06',
-                    'follower' => array('xuqian@domob.cn','lufugang@domob.cn'),
-                    'vote_num' => 100,
-                ),
-                'is_mine' => false,
-                'is_follower' => true,
-                'expire_time_left' => 1,
-            ),
-            array(
-                'petition' => array(
-                    'id' => 2,
-                    'title' => '请改善网络情况2',
-                    'desc' => '希望改善网络情况2',
-                    'owner' => 'lufugang@domob.cn',
-                    'create_time' => '2016-12-06',
-                    'follower' => array('xuqian@domob.cn','luotonglong@domob.cn'),
-                    'vote_num' => 100,
-                ),
-                'is_mine' => true,
-                'is_follower' => false,
-                'expire_time_left' => 1,
-            ),
-        );
-        echo $this->defaultSuccessJson($petitionInfoList);
     }
 }
